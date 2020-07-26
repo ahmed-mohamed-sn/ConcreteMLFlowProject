8598ff726188f4ca36ff7e7719935c25f2531243
ahmed.mohamed <ahmed.mohamed@leaseplan.com>
2020-07-26 13:53:39 +0200

    updated example

--- evaluate.py
+++ evaluate.py
@@ -4,26 +4,29 @@ import pandas as pd
 import mlflow
 from sklearn.metrics import mean_absolute_error, r2_score
 
-def evaluate_test_data_for_model(model, X_test, y_test):
-    y_pred = model.predict(X_test)
-    r2 = r2_score(y_test, y_pred)
-    mae = mean_absolute_error(y_test, y_pred)
-    print('R2: {}'.format(r2))
-    print('MAE: {}'.format(mae))
+def evaluate_test_data_for_model(y_true, y_pred):
+    r2 = r2_score(y_true, y_pred)
+    mae = mean_absolute_error(y_true, y_pred)
     return r2, mae
 
 if __name__ == "__main__":
     
     model = joblib.load('model.pkl')
     
-    file_path = sys.argv[1]
+    if len(sys.argv) > 2:
+        X_path = sys.argv[1]
+        y_path = sys.argv[2]
+    else:
+        X_path = 'X_sample.csv'
+        y_path = 'y_sample.csv'
     
     with mlflow.start_run(experiment_id=0):
-        sample_df = pd.read_csv(file_path)
+        X = pd.read_csv(X_path)
+        y = pd.read_csv(y_path)
     
-        y_pred = model.predict(sample_df)
+        y_pred = model.predict(X)
         
-        r2, mae = evaluate_test_data_for_model(rf_model, X_val, y_val)
+        r2, mae = evaluate_test_data_for_model(y, y_pred)
         
         mlflow.log_metric('r2', r2)
         mlflow.log_metric('mae', mae)
